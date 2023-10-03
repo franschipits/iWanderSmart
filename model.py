dc783fa6ccf92781812857df0ad799b378b94e1c
Franciane Schipits <franmifreitas@gmail.com>
2023-10-02 19:07:20 -0500

    in progress

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     """A user"""
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_name = db.Column(db.String)
@@ -17,42 +17,162 @@ class User(db.Model):
 
     saved_itineraries = db.relationship("Saved_Itinerary", back_populates="user")
     
-
     def __repr__(self):
         """Show info about the user"""
 
         return f"<User user_id={self.user_id} email={self.email}>"
 
 
-class Movie(db.Model):
-    """"A movie"""
-    __tablename__ = "movies"
 
-    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String)
-    overview = db.Column(db.Text)
-    release_date = db.Column(db.DateTime)
-    poster_path = db.Column(db.String)
+class User_Itinenary(db.Model):
+    """"A User Itinerary"""
+    __tablename__ = "user_itinerary"
 
-    ratings = db.relationship("Rating", back_populates="movie")
+    user_itinerary_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    creator = db.Column(db.Integer, db.ForeignKey("user.user_id"))
+    places_id = db.Column(db.Integer, db.ForeignKey("places.places_id"))
+
+    saved_itineraries = db.relationship("Saved_Itinerary", back_populates="user_itinerary")
+    
+    itinerary_activities = db.relationship("Itinerary_Activities", back_populates="user_itinerary")
+    stays = db.relationship("Stays", back_populates="user_itinerary")
+    flights = db.relationship("Flights", back_populates="user_itinerary")
+    places = db.relationship("Places", back_populates="user_itinerary")
 
     def __repr__(self):
-        """"Show info about the movie"""
+        """"Show info about the user itinerary"""
 
-        return f"<Movie movie_id={self.movie_id} title={self.title}>"
+        return f"<User_Itinerary user_itinerary_id={self.user_itinerary_id} creator={self.creator}>"
     
+
     
-class Rating(db.Model):
-    """"A movie rating"""
-    __tablename__ = "ratings"
+class Saved_Itinerary(db.Model):
+    """"The itineraries saved by the user"""
+    __tablename__ = "saved_itineraries"
 
-    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    score = db.Column(db.Integer)
-    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    saved_itineraries_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"))
+    user_itinerary_id = db.Column(db.Integer, db.ForeignKey("user_itinerary.user_itinerary_id"))
 
-    movie = db.relationship("Movie", back_populates="ratings")
-    user = db.relationship("User", back_populates="ratings")
+    user = db.relationship("User", back_populates="saved_itineraries")
+    user_itinerary = db.relationship("User_Itinerary", back_populates="saved_itineraries")
 
     def __repr__(self):
-        return f"<Rating rating_id={self.rating_id} score={self.score}>"
+        return f"<Saved_Itinerary saved_itineraries_id={self.saved_itineraries_id} user_itinerary={self.user_itinerary}>"
+    
+
+
+class Flights(db.Model):
+    """Information about the flights"""
+    __tablename__ = "flights"
+
+    flight_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_itinerary_id = db.Column(db.Integer, db.ForeignKey("user_itinerary.user_itinerary_id"))
+    type_flight = db.Column(db.String)
+    date_time = db.Column(db.DateTime)
+    price = db.Column(db.Float)
+
+    user_itinerary = db.relationship("User_Itinerary", back_populates="flights")
+
+    def __repr__(self):
+        return f"<Flights flight_id={self.flight_id} type_flight={self.type_flight}>"
+    
+
+
+class Itinerary_Activities(db.Model):
+    """Itinerary for things to do"""
+    __tablename__ = "itinerary_activities"
+
+    itinerary_activities_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_itinerary_id = db.Column(db.Integer, db.ForeignKey("user_itinerary.user_itinerary_id"))
+    activities_id = db.Column(db.Integer, db.ForeignKey("activities.activities_id"))
+    price = db.Column(db.Float)
+
+    user_itinerary = db.relationship("User_Itinerary", back_populates="itinerary_activities")
+    activities = db.relationship("Activities", back_populates="itinerary_activities")
+
+    def __repr__(self):
+        return f"<Itinerary_Actibities itinerary_activities_id={self.itinerary_activities_id} activities={self.activities}>"
+    
+
+
+class Activities(db.Model):
+    """Information about specific activities"""
+    __tablename__ = "activities"
+
+    activities_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    contact_info = db.Column(db.String)
+
+    itinerary_activities = db.relationship("Itinerary_Activities", back_populates="activities")
+
+    def __repr__(self):
+        return f"<Activities activities_id={self.activities_id} name={self.name}>"
+
+
+
+class Hotel(db.Model):
+    """Information about specific hotel"""
+    __tablename__ = "hotel"
+
+    hotel_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+    contact = db.Column(db.String)
+
+    stays = db.relationship("Stays", back_populates="hotel")
+
+    def __repr__(self):
+        return f"<Hotel hotel_id={self.hotel_id} name={self.name}>"
+    
+
+
+class Stays(db.Model):
+    """Information about the stay"""
+    __tablename__ = "stays"
+
+    stay_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    hotel_id = db.Column(db.Integer, db.ForeignKey("hotel.hotel_id"))
+    user_itinerary_id = db.Column(db.Integer, db.ForeignKey("user_itinerary.user_itinerary_id"))
+    price = db.Column(db.Float)
+    num_nights = db.Column(db.Integer)
+
+    user_itinerary = db.relationship("User_Itinerary", back_populates="stays")
+    hotel = db.relationship("Hotel", back_populates="stays")
+
+    def __repr__(self):
+        return f"<Stays stay_id={self.stay_id} hotel={self.hotel}>"
+    
+
+
+class Places(db.Model):
+    """Information about places to go"""
+    __tablename__ = "places"
+
+    places_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    country = db.Column(db.String)
+    state = db.Column(db.String)
+    city = db.Column(db.String)
+    passport = db.Column(db.Boolean)
+    Visa = db.Column(db.Boolean)
+
+    user_itinenary = db.relationship("User_Itinerary", back_populates="places")
+
+    def __repr__(self):
+        return f"<Places places_id={self.places_id} country={self.country}>"
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///travels", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+if __name__ == '__main__':
+    import server
+    connect_to_db(server.app)
